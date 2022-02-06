e39d8f37f27449bc1a17d17cb7761a43c3b0a81b
djamel <djadweb@gmail.com>
2022-02-06 16:53:00 +0100

    Redirect to a specified URL

--- script.js
+++ script.js
@@ -111,11 +111,20 @@
 
 //Reverse the order of the characters in the string
 
-const reverse = s => {
-    console.log(s.split('').reverse().join(''))
-}
+// const reverse = s => {
+//     console.log(s.split('').reverse().join(''))
+// }
 
-reverse("randomname")
+// reverse("randomname")
 
 
 //********************************************
+
+
+//Redirect to a specified URL
+
+// const redirect = (url) => {
+//     window.location.href = url;
+// }
+
+// redirect("https://www.google.com")
