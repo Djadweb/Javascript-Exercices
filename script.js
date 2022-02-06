f1e38d7b6071bb493ac58bb9fa7fe9752a0d3947
djamel <djadweb@gmail.com>
2022-02-06 16:00:46 +0100

    Get the nth element of a given array

--- script.js
+++ script.js
@@ -78,3 +78,17 @@
 
 
 //********************************************
+
+
+//Get the nth element of a given array
+
+const getElement = (array, n) => {
+    let element = array.slice(n-1, n);
+    console.log(element)
+}
+
+getElement([5, 2, 6, 88, 3, 102], 3);
+
+
+
+//********************************************
