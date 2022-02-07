721a3bb20d611c58795f4f369f0414ff6215d37e
djamel <djadweb@gmail.com>
2022-02-07 22:41:57 +0100

    Invokes the provided function after wait milliseconds

--- script.js
+++ script.js
@@ -190,13 +190,27 @@
 
 //15- Write a JavaScript program to calculate the factorial of a number.
 
-const fact = number => {
-    let factorial = 1;
-    for(let i=number; i>0; i--) {
-        factorial= factorial*i;
-    }
+// const fact = number => {
+//     let factorial = 1;
+//     for(let i=number; i>0; i--) {
+//         factorial= factorial*i;
+//     }    
 
-    console.log(factorial);
+//     console.log(factorial);
+// }
+
+
+//********************************************
+
+
+//16- Invokes the provided function after wait milliseconds
+
+const delayFunction = (fn, time) => {
+    setTimeout(fn, time);
 }
 
-fact(3);
+const display = () => {
+    window.alert('Delayed message');
+}
+
+delayFunction(display, 3000);
