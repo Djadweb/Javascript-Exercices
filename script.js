3277c49a15cf02d57b31b90ffc8535569de8add8
djamel <djadweb@gmail.com>
2022-02-07 22:53:47 +0100

    Create an element from a string

--- script.js
+++ script.js
@@ -226,3 +226,17 @@
 // }
 
 // getUrl()
+
+
+//********************************************
+
+//18- Create an element from a string
+
+// const create = (string) => {
+//     let element = document.createElement('div');
+//     element.innerHTML = string;
+
+//     return element.firstElementChild
+// }
+
+// create('<h1>Title</h1>');
